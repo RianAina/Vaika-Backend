3c0b3db9a1b7ed8b12e938e35ab265658d2dc4ac
Sarobidy <sarobidy.rakotonarivo.1@gmail.com>
2024-02-16 22:36:00 +0300

    update user controller

--- src/main/java/com/vaika/backend/controller/UserController.java
+++ src/main/java/com/vaika/backend/controller/UserController.java
@@ -1,15 +1,22 @@
 package com.vaika.backend.controller;
 
+import com.vaika.backend.entity.User;
+import com.vaika.backend.service.UserService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @Slf4j
+@AllArgsConstructor
 @RestController
 @RequestMapping
 public class UserController {
+    private UserService userService;
     @PostMapping("inscription")
-    public void inscription(){
+    public void inscription(@RequestBody User user){
         log.info("inscription");
+        this.userService.inscription(user);
     }
 }
