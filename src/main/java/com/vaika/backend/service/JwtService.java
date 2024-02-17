2d7ecef52455b041436f20a2263b7681c280c6fb
Sarobidy <sarobidy.rakotonarivo.1@gmail.com>
2024-02-17 14:45:44 +0300

    ajouter la fonctionnaliter JWT Service

--- src/main/java/com/vaika/backend/service/JwtService.java
+++ src/main/java/com/vaika/backend/service/JwtService.java
@@ -1,13 +1,48 @@
 package com.vaika.backend.service;
 
-import com.vaika.backend.dto.AuthentificationDTO;
+import com.vaika.backend.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
 import java.util.Map;
-
+@AllArgsConstructor
 @Service
 public class JwtService {
+    public final String ENCRIPTION_KEY = "b11f245de47fd67f0215b7e30f7bf4337c39e76fd87148305397344aa8a4c92f";
+    private UserService userService;
     public Map<String,String> generate(String username){
-        return null;
+        User user = (User) this.userService.loadUserByUsername(username);
+        return this.generateJwt(user);
+    }
+
+    private Map<String, String> generateJwt(User user) {
+        final Map<String, String> claims = Map.of(
+                "nom", user.getName(),
+                "email", user.getEmail()
+        );
+        final long currentTime = System.currentTimeMillis();
+        final long expirationTime = currentTime+ 30*60*1000;
+
+        final String token = Jwts.builder()
+                .setIssuedAt(new Date(currentTime))
+                .setExpiration(new Date(expirationTime))
+                .setSubject(user.getEmail())
+                .setClaims(claims)
+                .signWith(getKey(), SignatureAlgorithm.ES256)
+                .compact();
+        return Map.of("token",token);
+    }
+
+    private Key getKey() {
+        final byte[] decoder = Decoders.BASE64.decode(ENCRIPTION_KEY);
+        return Keys.hmacShaKeyFor(decoder);
     }
 }
+
+
